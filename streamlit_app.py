351a27ec0817c21d279d1eeab1e40b2318a1b8c7
snowflakeSarah <121889544+snowflakeSarah@users.noreply.github.com>
2023-01-04 10:50:19 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
-streamlit.text(fruityvice_response)
+
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
