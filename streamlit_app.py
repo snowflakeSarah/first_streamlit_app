4b175c71516c9e80d1ad4cc41c7fa91fe6913f92
snowflakeSarah <121889544+snowflakeSarah@users.noreply.github.com>
2023-01-04 10:51:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas
 import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -25,4 +26,4 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
